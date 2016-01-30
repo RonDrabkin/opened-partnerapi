bae8f7b54c711aac3a6cedc013adb0e13bafb882
lucascampbell <lkecamp1@gmail.com>
2016-01-29 17:12:10 -0800

    fix params

--- lib/opened/partnerapi.rb
+++ lib/opened/partnerapi.rb
@@ -2,9 +2,7 @@ require "opened/partnerapi/version"
 
 module Opened
   module Partnerapi
-    APP_SECRET = nil
-    CLIENT_ID  = nil
-
+    
     def self.included base
       base.extend ClassMethods
     end
@@ -14,14 +12,14 @@ module Opened
       def signed_request (params)
         check_for_errors(params)
         envelope              = params
-        envelope["client_id"] ||= CLIENT_ID
+        envelope["client_id"] ||= params[:client_id]
         envelope["algorithm"] ||= 'HMAC-SHA256'
         envelope["token"]     ||= SecureRandom.hex
         
         envelope          = JSON.dump(envelope)
         encoded_envelope  = base64_url_encode(envelope)
 
-        signature         = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, APP_SECRET, encoded_envelope)
+        signature         = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, params[:app_secret], encoded_envelope)
         encoded_signature = base64_url_encode(signature)
 
         return "#{encoded_signature}.#{encoded_envelope}"
@@ -34,10 +32,11 @@ module Opened
       end
 
       def check_for_errors (params)
-        raise Exception.new "Your Opened::Partnerapi::APP_SECRET is not set" unless APP_SECRET
-        raise Exception.new "Your Opened::Partnerapi::CLIENT_ID is not set" unless CLIENT_ID
+        raise Exception.new "Your must pass a client_id" unless params[:client_id]
+        raise Exception.new "Your must pass an app_secret" unless params[:app_secret]
         raise Exception.new "You must pass a username" unless params[:username]
       end
     end
+
   end
 end
